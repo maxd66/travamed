71610aa677b624727bed230d34b10a6a0910b39f
maxd66 <max.dunn63@gmail.com>
2022-04-30 02:53:27 -0700

    styled multiple form options

--- src/components/Form/Form.js
+++ src/components/Form/Form.js
@@ -1,8 +1,15 @@
 import "./Form.css";
 import React, { useState } from "react";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faEyeSlash, faEye } from "@fortawesome/free-solid-svg-icons";
 
 function Form() {
-  const [formState, setFormState] = useState({ fullName: "" });
+  const [formState, setFormState] = useState({
+    fullName: "",
+    email: "",
+    password: "",
+  });
+  const [passwordViewState, setPasswordViewState] = useState(false);
 
   function handleFormChange(e) {
     e.preventDefault();
@@ -14,22 +21,66 @@ function Form() {
     });
   }
 
+  function passwordViewHandler(e) {
+    e.preventDefault();
+    setPasswordViewState(!passwordViewState);
+  }
+
   function handleFormSubmit(e) {
     e.preventDefault();
-    console.log("you submitted the form");
+    alert(
+      `You submitted the form with the values, ${formState.fullName}, ${formState.email}, ${formState.password}`
+    );
   }
   return (
     <div>
-      <form onSubmit={handleFormSubmit}>
-        <label htmlFor="name" id="nameLabel">
-          Full Name
-        </label>
-        <input
-          id="fullName"
-          name="fullName"
-          onChange={handleFormChange}
-          value={formState.fullName}
-        />
+      <form id="formContainer" onSubmit={handleFormSubmit}>
+        <div id="firstInputContainer">
+          <input
+            id="fullName"
+            name="fullName"
+            className={formState.fullName ? "activeInput" : "inactiveInput"}
+            onChange={handleFormChange}
+            value={formState.fullName}
+          />
+          <label
+            htmlFor="name"
+            id="nameLabel"
+            className={
+              formState.fullName ? "activeNameLabel" : "inactiveNameLabel"
+            }
+          >
+            Full Name
+          </label>
+        </div>
+        <div className="inputContainer">
+          <label htmlFor="email" id="emailLabel">
+            Email
+          </label>
+          <input
+            id="email"
+            name="email"
+            onChange={handleFormChange}
+            value={formState.email}
+          />
+        </div>
+        <div className="inputContainer">
+          <input
+            id="password"
+            name="password"
+            type={passwordViewState ? "text" : "password"}
+            onChange={handleFormChange}
+            value={formState.password}
+            placeholder="Password"
+          />
+          <div id="eyeIconContainer" onClick={passwordViewHandler}>
+            {passwordViewState ? (
+              <FontAwesomeIcon id="eyeIcon" icon={faEyeSlash} />
+            ) : (
+              <FontAwesomeIcon id="eyeIcon" icon={faEye} />
+            )}
+          </div>
+        </div>
         <button id="submit-button" type="submit">
           Submit
         </button>
--- src/components/Nav/Nav.js
+++ src/components/Nav/Nav.js
@@ -1,10 +1,15 @@
 import "./Nav.css";
+import { Link } from "react-router-dom";
 
 function Nav() {
   return (
     <div id="nav-container">
-      <h2>logo</h2>
-      <button>Get Started</button>
+      <Link to="/">
+        <h2>Logo</h2>
+      </Link>
+      <Link to="/form">
+        <button>Get Started</button>
+      </Link>
     </div>
   );
 }
